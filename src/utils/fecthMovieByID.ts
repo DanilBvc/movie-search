bbd0e337399b7d0a454fc5b19910e4987b2d4cb7
Danil <danylo.makhov@nure.ua>
2023-03-09 13:39:21 +0300

    fix: change http fetch protocol to https

--- src/utils/fecthMovieByID.ts
+++ src/utils/fecthMovieByID.ts
@@ -1,6 +1,6 @@
 export const fetchMovieByID = async (id: string) => {
   const apiKey = '4a3b711b'
-  const url = `http://www.omdbapi.com/?i=${id}&plot=full&apikey=${apiKey}`
+  const url = `https://www.omdbapi.com/?i=${id}&plot=full&apikey=${apiKey}`
   const response = await fetch(url)
   const data = await response.json()
   return data
